b4959d4f5431dc9d330e0fbc5e7772321ce7958a
Joel Kandathil <joel.kandathil@soti.net>
2022-09-01 16:04:18 +0530

    Minor Update

--- Question4.cs
+++ Question4.cs
@@ -8,9 +8,14 @@ namespace ConsoleApp1
 {
     internal class Question4
     {
-        public static void Main2()
+        public static void Main()
         {
+            Console.WriteLine($"-1 + 4 * 6 = {-1 + 4 * 6}");
+            Console.WriteLine($"( 35+ 5 ) % 7 = {(35 + 5) % 7}");
+            Console.WriteLine($"14 + -4 * 6 / 11 = {14 + -4 * 6 / 11}");
+            Console.WriteLine($"2 + 15 / 6 * 1 - 7 % 2 = {2 + 15 / 6 * 1 - 7 % 2}");
 
+            Console.ReadKey();
         }
     }
 }
